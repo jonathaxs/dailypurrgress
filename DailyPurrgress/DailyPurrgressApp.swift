2ca6d4239470f66979150bd35d735e17ce2ebc4e
Jonathas Motta <jonathasmrt@gmail.com>
2026-02-12 22:57:39 -0300

    Inject HabitsStore into app environment

--- DailyPurrgress/DailyPurrgressApp.swift
+++ DailyPurrgress/DailyPurrgressApp.swift
@@ -4,9 +4,11 @@ import SwiftUI
 
 @main
 struct DailyPurrgressApp: App {
+    @StateObject private var habitsStore = HabitsStore()
     var body: some Scene {
         WindowGroup {
             TodayMiniView()
+                .environmentObject(habitsStore)
         }
     }
 }
