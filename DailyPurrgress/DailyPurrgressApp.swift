5c12366278231fd5312556e44020a97903c2dfb0
Jonathas Motta <jonathasmrt@gmail.com>
2026-02-23 02:10:28 -0300

    Warm up haptics engine at launch

--- DailyPurrgress/DailyPurrgressApp.swift
+++ DailyPurrgress/DailyPurrgressApp.swift
@@ -3,10 +3,16 @@
 //  Swift Student Challenge 2026
 
 import SwiftUI
+import CoreHaptics
 
 @main
 struct DailyPurrgressApp: App {
     @StateObject private var habitsStore = HabitsStore()
+
+    init() {
+        // Warm up the haptics engine early to avoid a first-tap delay (cold start).
+        HapticsWarmup.shared.prepare()
+    }
     var body: some Scene {
         WindowGroup {
             TodayMiniView()
